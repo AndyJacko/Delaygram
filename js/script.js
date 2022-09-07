84c1791ce72265e53ab2cf90a82051d4464d0fad
Andy Jacko <wisdom.mcr@gmail.com>
2022-09-07 10:55:44 +0100

    Created CSS/JS folders...added profile page...

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 const toggleDarkness = () => {
   if (
     document.getElementById("pagestyle").getAttribute("href") ===
-    "dark-styles.css"
+    "css/dark-styles.css"
   ) {
-    document.getElementById("pagestyle").setAttribute("href", "styles.css");
+    document.getElementById("pagestyle").setAttribute("href", "css/styles.css");
     document.getElementById("logo-img").setAttribute("src", "images/logo.png");
     document.getElementById("theme").textContent = "Dark Theme";
     document.getElementById("profile-menu").classList.add("hide");
   } else {
     document
       .getElementById("pagestyle")
-      .setAttribute("href", "dark-styles.css");
+      .setAttribute("href", "css/dark-styles.css");
     document.getElementById("logo-img").setAttribute("src", "images/logo2.png");
     document.getElementById("theme").textContent = "Light Theme";
     document.getElementById("profile-menu").classList.add("hide");
