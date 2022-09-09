5561661b4fda0a5c41dcee914c38abf38764e230
Andy Jacko <wisdom.mcr@gmail.com>
2022-09-09 16:20:56 +0100

    Finalish edition...

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,48 @@
 const toggleDarkness = () => {
   if (document.getElementById("pagestyle").getAttribute("href") === "css/dark.css") {
     document.getElementById("pagestyle").setAttribute("href", "css/styles.css");
-    document.getElementById("logo-img").setAttribute("src", "images/logo.png");
+    if (document.getElementById("logo-img-sm")){
+      document.getElementById("logo-img-sm").setAttribute("src", "images/logo.png");
+    }
+    if (document.getElementById("logo-img-lg")){
+      document.getElementById("logo-img-lg").setAttribute("src", "images/logo.png");
+    }
     document.getElementById("theme").textContent = "Dark Theme";
-    document.getElementById("profile-menu").classList.add("hide");
+    document.getElementById("profile-menu-sm").classList.add("hide");
+    document.getElementById("profile-menu-lg").classList.add("hide");
   } else {
     document.getElementById("pagestyle").setAttribute("href", "css/dark.css");
-    document.getElementById("logo-img").setAttribute("src", "images/logo2.png");
+    if (document.getElementById("logo-img-sm")) {
+      document.getElementById("logo-img-sm").setAttribute("src", "images/logo2.png");
+    }
+    if (document.getElementById("logo-img-lg")) {
+      document.getElementById("logo-img-lg").setAttribute("src", "images/logo2.png");
+    }
     document.getElementById("theme").textContent = "Light Theme";
-    document.getElementById("profile-menu").classList.add("hide");
+    document.getElementById("profile-menu-sm").classList.add("hide");
+    document.getElementById("profile-menu-lg").classList.add("hide");
   }
 };
 
-const toggleProfileMenu = () => {
-  document.getElementById("profile-menu").classList.toggle("hide");
+const toggleProfileMenuLg = () => {
+  document.getElementById("profile-menu-lg").classList.toggle("hide");
 };
 
-document.getElementById("pp").addEventListener("click", (e) => {
-  toggleProfileMenu();
+const toggleProfileMenuSm = () => {
+  document.getElementById("profile-menu-sm").classList.toggle("hide");
+};
+
+document.getElementById("pp-sm").addEventListener("click", (e) => {
+  toggleProfileMenuSm();
+});
+
+document.getElementById("pp-lg").addEventListener("click", (e) => {
+  toggleProfileMenuLg();
 });
 
 document.querySelector("main").addEventListener("click", (e) => {
-  document.getElementById("profile-menu").classList.add("hide");
+  document.getElementById("profile-menu-sm").classList.add("hide");
+  document.getElementById("profile-menu-lg").classList.add("hide");
 });
 
 document.getElementById("p-link").addEventListener("click", (e) => {
